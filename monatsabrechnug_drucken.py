d71bbb39dfcf5dd86d4ce576e25d897b63f58d77
Raphael Kaiser <raphaelricokaiser@web.de>
2020-08-24 23:48:13 +0200

    bug_fix_runs_now

--- monatsabrechnug_drucken.py
+++ monatsabrechnug_drucken.py
@@ -26,37 +26,82 @@ liste  = [fix_sum]
 liste2 = [gesamtkosten_ohne_fixkosten]
 liste3 = [gesamtkosten]
 
+#check
+print(np.shape(porto))
+print(porto)
+print(liste3)
 
-#Tab 1
-tab1 = TexTable([porto, buero ,stuff],
-                        [r'Portokosten / \euro',
-                         r'Büroartikel / \euro',
-                         r'Sonstige Ausgaben / \euro'],
-                        label='monatsabrechnung_1',
-                        caption='Tabelle der variablen Kosten.'#TODO mit date 
-                )
+try:
+        #Tab 1
+        tab1 = TexTable([porto, buero ,stuff],
+                                [r'Portokosten / \euro',
+                                 r'Büroartikel / \euro',
+                                 r'Sonstige Ausgaben / \euro'],
+                                label='monatsabrechnung_1',
+                                caption='Tabelle der variablen Kosten.'#TODO mit date 
+                        )
 
-tab1.setRowRounding(0,2)
-tab1.setRowRounding(1,2)
-tab1.setRowRounding(2,2)
-tab1.writeFile('build/tab_monatsabrechnung_drucken_part_1.tex')
-
-
-#Tab 2
-tab2 = TexTable([liste, liste2 ,liste3],
-                        [r'Fixkosten / \euro',
-                         r'Gesamtkosten (o.F.) / \euro',
-                         r'Gesamtkosten / \euro'],
-                        label='monatsabrechnung_2',
-                        caption='Tabelle der Fix- und Gesamtkosten.'
-                )
-
-tab2.setRowRounding(0,2)
-tab2.setRowRounding(1,2)
-tab2.setRowRounding(2,2)
-tab2.writeFile('build/tab_monatsabrechnung_drucken_part_2.tex')
+        tab1.setRowRounding(0,2)
+        tab1.setRowRounding(1,2)
+        tab1.setRowRounding(2,2)
+        tab1.writeFile('build/tab_monatsabrechnung_drucken_part_1.tex')
 
 
-#executes the given command in the terminal
-os.system('touch protokoll.tex') #lel ich hab nicht ganz verstanden warum das damit jetzt geht XD
-os.system('make')
+        #Tab 2
+        tab2 = TexTable([liste, liste2 ,liste3],
+                                [r'Fixkosten / \euro',
+                                 r'Gesamtkosten (o.F.) / \euro',
+                                 r'Gesamtkosten / \euro'],
+                                label='monatsabrechnung_2',
+                                caption='Tabelle der Fix- und Gesamtkosten.'
+                        )
+
+        tab2.setRowRounding(0,2)
+        tab2.setRowRounding(1,2)
+        tab2.setRowRounding(2,2)
+        tab2.writeFile('build/tab_monatsabrechnung_drucken_part_2.tex')
+
+
+        #executes the given command in the terminal
+        os.system('touch protokoll.tex') #lel ich hab nicht ganz verstanden warum das damit jetzt geht XD
+        os.system('make')
+except TypeError:
+
+        #because it does not make it a list if it is only one value
+        porto = [porto]
+        buero = [buero]
+        stuff = [stuff]
+
+         #Tab 1
+        tab1 = TexTable([porto, buero ,stuff],
+                                [r'Portokosten / \euro',
+                                 r'Büroartikel / \euro',
+                                 r'Sonstige Ausgaben / \euro'],
+                                label='monatsabrechnung_1',
+                                caption='Tabelle der variablen Kosten.'#TODO mit date 
+                        )
+
+        tab1.setRowRounding(0,2)
+        tab1.setRowRounding(1,2)
+        tab1.setRowRounding(2,2)
+        tab1.writeFile('build/tab_monatsabrechnung_drucken_part_1.tex')
+
+
+        #Tab 2
+        tab2 = TexTable([liste, liste2 ,liste3],
+                                [r'Fixkosten / \euro',
+                                 r'Gesamtkosten (o.F.) / \euro',
+                                 r'Gesamtkosten / \euro'],
+                                label='monatsabrechnung_2',
+                                caption='Tabelle der Fix- und Gesamtkosten.'
+                        )
+
+        tab2.setRowRounding(0,2)
+        tab2.setRowRounding(1,2)
+        tab2.setRowRounding(2,2)
+        tab2.writeFile('build/tab_monatsabrechnung_drucken_part_2.tex')
+
+
+        #executes the given command in the terminal
+        os.system('touch protokoll.tex') #lel ich hab nicht ganz verstanden warum das damit jetzt geht XD
+        os.system('make')
